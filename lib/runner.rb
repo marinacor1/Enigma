a8ebc811694b39d88bb3b3448eacb09976b41e76
Marina Corona <marina.c.corona@gmail.com>
2015-12-14 08:45:46 -0700

    first commit

--- lib/runner.rb
+++ lib/runner.rb
@@ -6,8 +6,8 @@ class Runner
 #adds stuff
 #gets four numbers
   def initialize(key_array)
-    @key_array
-    @date
+    @key_array = []
+    @date = date
   end
 
   def key_to_abcd(key_array)
@@ -21,28 +21,29 @@ class Runner
   def date_to_number_format
     #look at pragmatic studio
     t = Time.now
-    date = t.strftime("%m%d%y")
+    @date = t.strftime("%m%d%y")
   end
 
-  def square_date(new_date)
-    new_date = date.to_i ** 2
+  def square_date(date)
+    @new_date = @date.to_i ** 2
   end
 
-  def square_to_offset
+  def square_to_offset(new_date)
+    #get the last four digits for the offset
     #Fixednum this wont' work. could turn it into an array
     #or find other method for fixed num
-    d_2 = new_date[-1]
-    c_2 = new_date[-2]
-    b_2 = new_date[-3]
-    a_2 = new_date[-4]
+    d_2 = @new_date.to_s[-1]
+    c_2 = @new_date.to_s[-2]
+    b_2 = @new_date.to_s[-3]
+    a_2 = @new_date.to_s[-4]
 #this wont' work
   end
 
   def add_abcds
-   @final_a = @a_1 + @a_2
-   @final_b = @b_1 + @b_2
-   @final_c = @c_1 + @c_2
-   @final_d = @d_1 + @d_2
+   @rotation_a = @a_1.to_i + @a_2.to_i
+   @rotation_b = @b_1.to_i + @b_2.to_i
+   @rotation_c = @c_1.to_i + @c_2.to_i
+   @rotation_d = @d_1.to_i + @d_2.to_i
   end
 
   def encrypt(message)
@@ -51,15 +52,58 @@ class Runner
     # alphabet.index(i)
     # end
     message.split(//)
-    @key_array = message.gsub(/[abcdefghijklmnopqrstuvwxyz]/, 'a' => '0 ', 'b' => '1 ', 'c' => '2 ', 'd' => '3 ', 'e' => '4 ', 'f' => '5 ', 'g' => '6 ', 'h' => '7 ', 'i' => '8 ', 'j' => '9 ', 'k' => '10 ', 'l' => '11 ', 'm' => '12 ', 'n' => '13 ', 'o' => '14 ', 'p' => '15 ', 'q' => '16 ', 'r' => '17 ', 's' => '18 ', 't' => '19 ', 'u' => '20 ', 'v' => '21 ', 'w' => '22 ', 'x' => '23 ', 'y' => '24 ', 'z' => '25 ', '0' => '26', '1' => '27', '2' => '28', '3' => '29', '4' => '30', '5' => '31', '6' => '32', '7' => '33', '8' => '34', '9' => '35', ' ' => '36', '.' => '37', ',' => '38')
+    @encrypted_message = message.gsub(/[abcdefghijklmnopqrstuvwxyz]/, 'a' => '0 ', 'b' => '1 ', 'c' => '2 ', 'd' => '3 ', 'e' => '4 ', 'f' => '5 ', 'g' => '6 ', 'h' => '7 ', 'i' => '8 ', 'j' => '9 ', 'k' => '10 ', 'l' => '11 ', 'm' => '12 ', 'n' => '13 ', 'o' => '14 ', 'p' => '15 ', 'q' => '16 ', 'r' => '17 ', 's' => '18 ', 't' => '19 ', 'u' => '20 ', 'v' => '21 ', 'w' => '22 ', 'x' => '23 ', 'y' => '24 ', 'z' => '25 ', '0' => '26', '1' => '27', '2' => '28', '3' => '29', '4' => '30', '5' => '31', '6' => '32', '7' => '33', '8' => '34', '9' => '35', ' ' => '36', '.' => '37', ',' => '38')
   end
 
-  def rotate
-
+  def abcd_assignment
+   rotation_array =["a", "b", "c", "d"]
+    @encrypted_array = @encrypted_message.split(" ")
+    index = 0
+    index_2 = 0
+      while index < @encrypted_array.length
+        @encrypted_array[index] = @encrypted_array[index].insert( -1, @rotation_array[index])
+        index +=1
+        if index_2 == 4
+        index_2 = 0
+      else index_2 +=1
+        end
+      end
   end
 
-  def reverse_rotate
+  def add_abcds_again_rotate()
+    large_index = 0
+    index = 0
+    @final_offsets = []
 
+    if large_index < @encrypted_array.length
+      if @encrypted_array[index].include?("a")
+        temp = @encrypted_array[index].delete("a")
+        temp = temp.to_i + @rotation_a
+      elsif @encrypted_array[index].include?("b")
+        temp = @encrypted_array[index].delete("b")
+        temp = temp.to_i + @rotation_b
+      elsif @encrypted_array[index].include?("c")
+        temp = @encrypted_array[index].delete("c")
+        temp = temp.to_i + @rotation_c
+      else
+        temp = @encrypted_array[index].delete("d")
+        temp = temp.to_i + @rotation_d
+      end
+      @final_offsets.push(temp)
+      large_index += 1
+    end
+  end
+
+  def numbers_to_letters(final_offsets)
+    @encryption = []
+    @final_offsets.each do |i|
+      encrypted_character = i%39
+      @encryption << encrypted_character
+    end
+  end
+
+  def decrypt
+    @encrypted_message = encryption.reverse.gsub(/[abcdefghijklmnopqrstuvwxyz]/, 'a' => '0 ', 'b' => '1 ', 'c' => '2 ', 'd' => '3 ', 'e' => '4 ', 'f' => '5 ', 'g' => '6 ', 'h' => '7 ', 'i' => '8 ', 'j' => '9 ', 'k' => '10 ', 'l' => '11 ', 'm' => '12 ', 'n' => '13 ', 'o' => '14 ', 'p' => '15 ', 'q' => '16 ', 'r' => '17 ', 's' => '18 ', 't' => '19 ', 'u' => '20 ', 'v' => '21 ', 'w' => '22 ', 'x' => '23 ', 'y' => '24 ', 'z' => '25 ', '0' => '26', '1' => '27', '2' => '28', '3' => '29', '4' => '30', '5' => '31', '6' => '32', '7' => '33', '8' => '34', '9' => '35', ' ' => '36', '.' => '37', ',' => '38')
   end
 end
 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,102 +1,85 @@
-gem 'minitest', '~> 5.2'                          # => true
-require 'minitest/autorun'                        # => true
-require 'minitest/pride'                          # => true
-require_relative '../lib/key_generating_machine'  # => true
-require_relative '../lib/runner'                  # => true
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/key_generating_machine'
+require_relative '../lib/runner'
 
-class EnigmaTest < Minitest::Test  # => Minitest::Test
+class EnigmaTest < Minitest::Test
 
 
 #Random 5 digit key is generated
   def test_key_generator_is_random
     # skip
-    keyarray_1 = KeyGeneratingMachine.new         # => #<KeyGeneratingMachine:0x007fa59983d060>
-    keyarray_2 = KeyGeneratingMachine.new         # => #<KeyGeneratingMachine:0x007fa59983cc78>
-    assert_equal false, keyarray_1 == keyarray_2  # => true
+    keyarray_1 = KeyGeneratingMachine.new
+    keyarray_2 = KeyGeneratingMachine.new
+    assert_equal false, keyarray_1 == keyarray_2
      #Test= Encrypt.new("55744")
-  end                                             # => :test_key_generator_is_random
+  end
 
 #original five digit number is converted to ABCD format
   def test_converts_five_digit_key_to_ABCD_format
     skip
-    key_array = KeyGeneratingMachine.new
-    Runner.key_to_abcd(key_array)
-    assert_equal 2, @a_1.length
-  end                                              # => :test_converts_five_digit_key_to_ABCD_format
+    # key_array = KeyGeneratingMachine.new
+    variable = Runner.new(["0", "1", "2", "3", "4"])
+    variable.key_to_abcd(["0", "1", "2", "3", "4"])
+    assert_equal 2, variable.count
+  end
 
 #Calculates date by number
   def test_date_is_converted_to_number_form
-    # skip
-    date = Runner.new(["0", "1", "2", "3", "4"])                                                # => #<Runner:0x007fa59984e810>
-    assert_equal false, date.date_to_number_format.include?(".")  # => true
-  end                                                             # => :test_date_is_converted_to_number_form
+    #skip
+    date = Runner.new(["0", "1", "2", "3", "4"])
+    assert_equal false, date.date_to_number_format.include?(".")
+  end
 
 #Date is squared
   def test_date_is_squared
     # skip
-    date = Runner.new(["0", "1", "2", "3", "4"])                                       # => 123085
+    date = 123085
     assert_equal "15149917225", date.square_date("123085")
-  end                                                   # => :test_date_is_squared
+  end
 
 #Last four digits of squared number become ABCD
   def test_that_ABCD_is_squared_numbers_last_four_digits
-    #how does it recognize the class?
     skip
-    new_square = 15149917225
-    assert_equal 7225, new_square.square_to_offset
-  end                                                     # => :test_that_ABCD_is_squared_numbers_last_four_digits
+    date = Runner.new(["0", "1", "2", "3", "4"])
+    assert_equal 7225, @new_date.square_to_offset(15149917225)
+  end
 
 
 #two ABCD numbers are added together
   def test_add_two_ABCD_numbers_together
     skip
-    final_a = Runner.new(["0", "1", "2", "3", "4"])   
+    final_a = Runner.new(["0", "1", "2", "3", "4"])
     assert_equal 52, final_a.add_abcds
-  end                                     # => :test_add_two_ABCD_numbers_together
+  end
 
   #A letter is properly organized into its number equivalent
   def test_properly_organizes_a_character_into_numbers
-    message = Runner.new(["0", "1", "2", "3", "4"])                                   # => #<Runner:0x007fa599855ca0>
-    assert_equal "0 ", message.encrypt("a")             # => true
-  end                                                   # => :test_properly_organizes_a_character_into_numbers
+    skip
+    message = Runner.new(["0", "1", "2", "3", "4"])
+    assert_equal "0 ", message.encrypt("a")
+  end
 
   #A message is properly organized into its number equivalent
   def test_properly_organizes_a_message_into_numbers
     skip
     message = Runner.new("an apple")
     assert_equal "0 13 36 0 15 15 11 4 ", message.encrypt("an apple")
-  end                                                                  # => :test_properly_organizes_a_message_into_numbers
+  end
 
 #message in character form is organized in ABCD format
   def test_organizes_character_message_in_ABCD_format
     skip
     numbered_message = ["7", "14", "17", "0", "2", "4"]
     assert_equal ["57", "31", "71", "26", "52", "21"], numbered_message.rotate
-  end                                                                           # => :test_organizes_character_message_in_ABCD_format
+  end
 
 #message is converted into encrytped message using remainder of 39 character system
   def test_message_is_encrypted_thru_39_character_system
     skip
     numbered_message = ["57", "31", "71", "26", "52", "21"]
-    assert_equal ["s", "7", "0", "n", "r"], numbered_message.reverse_rotate
-  end                                                                        # => :test_message_is_encrypted_thru_39_character_system
+    assert_equal ["s", "7", "0", "n", "r"], numbered_message.numbers_to_letters
+  end
 
-end  # => :test_message_is_encrypted_thru_39_character_system
-
-# >> 7 14 17 0 2 4
-# >> Run options: --seed 14754
-# >>
-# >> # Running:
-# >>
-# >> \e[41m\e[37mE\e[0m\e[31m*\e[0m\e[32mS\e[0m\e[33m*\e[0m\e[34mS\e[0m\e[35mS\e[0m\e[36mS\e[0m\e[31m*\e[0m\e[32mS\e[0m\e[33mS\e[0m
-# >>
-# >> \e[31mF\e[0m\e[32ma\e[0m\e[33mb\e[0m\e[34mu\e[0m\e[35ml\e[0m\e[36mo\e[0m\e[31mu\e[0m\e[32ms\e[0m\e[33m \e[0m\e[34mr\e[0m\e[35mu\e[0m\e[36mn\e[0m in 0.001823s, 5485.4635 runs/s, 1645.6391 assertions/s.
-# >>
-# >>   1) Error:
-# >> EnigmaTest#test_date_is_squared:
-# >> NoMethodError: undefined method `square_date' for 123085:Fixnum
-# >>     /Users/marinacorona/Turing/Module1/EnigmaProject/test/enigma_test.rb:38:in `test_date_is_squared'
-# >>
-# >> 10 runs, 3 assertions, 0 failures, 1 errors, 6 skips
-# >>
-# >> You have skipped tests. Run with --verbose for details.
+end
