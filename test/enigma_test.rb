f1e17339140321d9f6281161cfb9d9df88eb92da
Marina Corona <marina.c.corona@gmail.com>
2015-12-16 23:00:42 -0700

    decrypted

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,6 +5,7 @@ require 'minitest/pride'
 require 'pry'
 require_relative '../lib/key_generating_machine'
 require_relative '../lib/runner'
+require 'simplecov'
 SimpleCov.start
 
 class EnigmaTest < Minitest::Test
@@ -16,13 +17,10 @@ class EnigmaTest < Minitest::Test
     keyarray_1 = KeyGeneratingMachine.new
     keyarray_2 = KeyGeneratingMachine.new
     assert_equal false, keyarray_1 == keyarray_2
-     #Test= Encrypt.new("55744")
   end
 
 #original five digit number is converted to ABCD format
   def test_converts_five_digit_key_to_ABCD_format
-    # skip
-    # key_array = KeyGeneratingMachine.new
     runner = Runner.new(["0", "1", "2", "3", "4"])
     many_as = runner.key_to_abcd
     a_1 = many_as[0]
@@ -50,21 +48,19 @@ class EnigmaTest < Minitest::Test
     new_date = runner.square_date("123085")
     offsets = runner.square_to_offset(new_date)
     assert_equal "7", offsets[0]
-    assert_equal "2", offsets[1] #don't need to make instance here
+    assert_equal "2", offsets[1]
   end
 
 
 #two ABCD numbers are added together
   def test_add_two_ABCD_numbers_together
     final_a = Runner.new(["0", "1", "2", "3", "4"])
-    many_as = final_a.key_to_abcd
+    key_rotations = final_a.key_to_abcd
     new_date = final_a.square_date("123085")
     date_rotations = final_a.square_to_offset(new_date)
-    final_a.add_abcds(many_as, date_rotations)
-    #assert_equal 8, final_a.rotation_a
-    assert_equal 39, final_a.rotation_d
-                     rotation_array[0] + final_a
-    #in order for rotation_a to happen, all these other methods need to happen
+    final_abcd = final_a.add_abcds(key_rotations, date_rotations)
+    assert_equal 39, final_abcd[3]
+
   end
 
   #A letter is properly organized into its number equivalent
@@ -85,39 +81,32 @@ class EnigmaTest < Minitest::Test
   def test_organizes_character_message_in_ABCD_format
     # skip
     message = Runner.new(["0", "1", "2", "3", "4"])
-    many_as = message.key_to_abcd
-    new_date = message.square_date("123085")
-    date_rotations = message.square_to_offset(new_date)
-    message.add_abcds(many_as, date_rotations)
-    message.encrypt("apple")
-    message.abcd_assignment("apple")
-    assert_equal ["0a", "15b", "15c", "11d", "4a"], message.abcd_assignment("apple")
+    assert_equal ["0a", "15b", "15c", "11d", "4a"], message.abcd_assignment(message.encrypt("apple"))
   end
 
 
   def test_characters_have_abcd_deleted
     message = Runner.new(["0", "1", "2", "3", "4"])
-    message.addition_manager("123085")
-    message.encrypt("apple")
-    message.abcd_assignment("apple")
-    assert_equal ["8", "29", "40", "50", "12"], message.add_abcds_again_rotate
+    message.abcd_assignment(message.encrypt("apple"))
+    key_rotations = message.key_to_abcd
+    date_rotations =message.square_to_offset("123085")
+    final_abcd = message.add_abcds(key_rotations, date_rotations)
+    new_encrypted_array = message.abcd_assignment(message.encrypt("apple"))
+    message.add_abcds_again_rotate(final_abcd, new_encrypted_array)
   end
-#message is converted into encrytped message using remainder of 39 character system
+
 
   def test_message_is_encrypted_thru_39_character_system
     # skip
     message = Runner.new(["0", "1", "2", "3", "4"])
     message.addition_manager("123085")
-    message.encrypt("apple")
-    message.abcd_assignment("apple")
-    message.add_abcds_again_rotate
     assert_equal ["i", "3", "b", "l", "m"], message.numbers_to_letters(["8", "29", "40", "50", "12"])
   end
 
   def test_decrypts_encrypted_message
     # skip
     message = Runner.new(["0", "1", "2", "3", "4"])
-    assert_equal "apple", message.decrypt(["i", "3", "b", "l", "m"])
+    assert_equal "apple", message.decrypt(["i", "3", "b", "l", "m"], [8, 14, 25, 39])
   end
 
 end
